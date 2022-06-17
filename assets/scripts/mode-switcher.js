27ae2a2f8c12203f715a0583a4921689aa6ddc06
agerhardsson <andreasgerhardsson@gmail.com>
2022-06-17 10:07:36 +0200

    mainly icons

--- assets/scripts/mode-switcher.js
+++ assets/scripts/mode-switcher.js
@@ -2,19 +2,19 @@ let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)");
 let theme = sessionStorage.getItem('theme');
 
 if (systemInitiatedDark.matches) {
-	document.getElementById("theme-toggle").innerHTML = "<i class='fa fa-sun-o'></i>";
+	document.getElementById("theme-toggle").innerHTML = "<i class='material-icons md-18'>light_mode</i>";
 } else {
-	document.getElementById("theme-toggle").innerHTML = "<i class='fa fa-moon-o'></i>";
+	document.getElementById("theme-toggle").innerHTML = "<i class='material-icons md-18'>dark_mode</i>";
 }
 
 function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
   	document.documentElement.setAttribute('data-theme', 'dark');
-   	document.getElementById("theme-toggle").innerHTML = "<i class='fa fa-sun-o'></i>";
+   	document.getElementById("theme-toggle").innerHTML = "<i class='material-icons md-18'>light_mode</i>";
    	sessionStorage.setItem('theme', '');
   } else {
   	document.documentElement.setAttribute('data-theme', 'light');
-    document.getElementById("theme-toggle").innerHTML = "<i class='fa fa-moon-o'></i>";
+    document.getElementById("theme-toggle").innerHTML = "<i class='material-icons md-18'>dark_mode</i>";
     sessionStorage.setItem('theme', '');
   }
 }
@@ -26,28 +26,31 @@ function modeSwitcher() {
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("theme-toggle").innerHTML = "<i class='fa fa-moon-o'></i>";
+		document.getElementById("theme-toggle").innerHTML = "<i class='material-icons md-18'>dark_mode</i>";
 	}	else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = "<i class='fa fa-sun-o'></i>";
+		document.getElementById("theme-toggle").innerHTML = "<i class='material-icons md-18'>light_mode</i>";
 	} else if (systemInitiatedDark.matches) {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("theme-toggle").innerHTML = "<i class='fa fa-moon-o'></i>";
+		document.getElementById("theme-toggle").innerHTML = "<i class='material-icons md-18'>dark_mode</i>";
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = "<i class='fa fa-sun-o'></i>";
+		document.getElementById("theme-toggle").innerHTML = "<i class='material-icons md-18'>light_mode</i>";
 	}
 }
 
 if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', 'dark');
-	a = document.getElementById("theme-toggle").innerHTML = "<i class='fa fa-sun-o'></i>";
+	a = document.getElementById("theme-toggle").innerHTML = "<i class='material-icons md-18'>light_mode</i>";
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
-	document.getElementById("theme-toggle").innerHTML = "<i class='fa fa-moon-o'></i>";
+	document.getElementById("theme-toggle").innerHTML = "<i class='material-icons md-18'>dark_mode</i>";
 }
+// <span class="material-symbols-outlined">
+// light_mode
+// </span>
